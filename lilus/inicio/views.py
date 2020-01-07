c8d500874800d6572a07fc357c0284ddc5ba8c5b
Raul Siguenza <raulfan2000@gmail.com>
2020-01-07 22:50:07 +0100

    s

--- lilus/inicio/views.py
+++ lilus/inicio/views.py
@@ -4,10 +4,21 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 
+
+###### información ######
 def inicio(request):
     return render(request, 'inicio/inicio.html')
+
 def contacto(request):
     return render(request, 'inicio/contacto.html')
+
+def buscador(request):
+    return render(request, 'inicio/buscador.html')
+
+
+###### comunión ######
+def comunion2020(request):
+    return render(request, 'inicio/comunion2020.html')
 
 def comunion2019(request):
     return render(request, 'inicio/comunion2019.html')
@@ -15,14 +26,17 @@ def comunion2019(request):
 def comunion2018(request):
     return render(request, 'inicio/comunion2018.html')
 
+###### verano ######
 def verano2019(request):
     return render(request, 'inicio/verano2019.html')
 
 def verano2018(request):
     return render(request, 'inicio/verano2018.html')
 
+###### bautizo ######
 def bautizo(request):
     return render(request, 'inicio/bautizo.html')
 
+###### eventos ######
 def eventoValencia2019(request):
     return render(request, 'inicio/eventoValencia2019.html')
