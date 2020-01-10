ebf1f3741221f76e4015a1ee70324c9cb2fe971b
Raul Siguenza <raulfan2000@gmail.com>
2020-01-10 02:55:25 +0100

    dd

--- lilus/lilus/settings.py
+++ lilus/lilus/settings.py
@@ -75,16 +75,11 @@ WSGI_APPLICATION = 'lilus.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "lilus",
-        "USER": "",
-        "PASSWORD": "",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
