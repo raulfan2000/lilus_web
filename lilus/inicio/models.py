932dc56595468c13f2b12b298ed437e8a9f6c1b4
Raul Siguenza <rss75@alu.ua.es>
2021-07-28 13:07:15 +0200

    dd

---
+++ lilus/inicio/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
+from django.db import models
+from django.core.files import File
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
+from PIL import Image
+import uuid
+
+
+# colecciones
+class Coleccion(models.Model):
+    class Meta:
+        verbose_name = "Colección"
+        verbose_name_plural = "Colecciones"
+    # opciones de tipo de coleccion
+    CHOICES = (('Evento', 'Evento'),('Comunion', 'Comunión'), ('Bautizo', 'Bautizo'), ('Verano', 'Verano'))
+    # campos
+    id = models.UUIDField(_("ID"), primary_key=True, default=uuid.uuid4, editable=False)
+    creacion = models.DateTimeField(auto_now_add=True, blank=True)
+    nombre_real = models.CharField(max_length=32, default='')
+    nombre = models.CharField(max_length=32, default='')
+    tipo = models.CharField(max_length=32, choices=CHOICES, default='Evento')
+    anyo = models.IntegerField(default=2020)
+    activa = models.BooleanField(default=False)
+    # nombre en bbdd
+    def __str__ (self):
+        return f'{str(self.nombre)}'
+
+# fotos
+class Foto(models.Model):
+    class Meta:
+        verbose_name = "Foto"
+        verbose_name_plural = "Fotos"
+    # campos
+    id = models.UUIDField(_("ID"), primary_key=True, default=uuid.uuid4, editable=False)
+    creacion = models.DateTimeField(auto_now_add=True, blank=True)
+    imagen = models.ImageField(default='default.jpg', upload_to='galeria_imagenes')
+    nombre = models.CharField(max_length=32, default='00000')
+    # coleccion
+    coleccion = models.ForeignKey(Coleccion, on_delete=models.SET_NULL, null=True)
+    # guardar foto en la bbdd
+    def save(self, force_insert=False, force_update=False, using=None):
+        super().save()
+        img1 = Image.open(self.imagen.path)
+        if img1.height > 1400 or img1.width > 1400 :
+            output_size = (1400,1400)
+            img1.thumbnail((output_size), Image.ANTIALIAS)
+            img1.save(self.imagen.path)
+    def __str__ (self):
+        return f'{str(self.coleccion)} {str(self.nombre)}'
+
+# contacto
+class Contacto(models.Model):
+    class Meta:
+        verbose_name = "Formulario de contacto"
+        verbose_name_plural = "Formularios de contacto"
+    # campos
+    id = models.UUIDField(_("ID"), primary_key=True, default=uuid.uuid4, editable=False)
+    creacion = models.DateTimeField(auto_now_add=True, blank=True)
+    nombre = models.CharField(max_length=32, default='')
+    apellidos = models.CharField(max_length=120, default='')
+    correo = models.CharField(max_length=120, default='')
+    pais = models.CharField(max_length=120, default='')
+    prefijo = models.CharField(max_length=120, default='')
+    telefono = models.IntegerField(default=000000000)
+    idioma = models.CharField(max_length=120, default='')
+    mensaje = models.CharField(max_length=5000, default='')
+    # nombre en bbdd
+    def __str__ (self):
+        return f'{str(self.nombre)}'
---
+++ lilus/inicio/static/inicio/main.js
@@ -0,0 +1,106 @@
+$(window).scroll(function(){
+	if ($(this).scrollTop() > 77 && screen.width > 900){
+		$('#menu').addClass("menuVisible");
+		$('#nav').addClass("navegadorDivScroll");
+		$('#nav').addClass("fixed-top");
+		$('#fondo').addClass("marginScroll");
+		$('#activeBusquedaScroll, .fa-globe-europe, .idiomaDiv, .fa-caret-down, .facebookLink, .logoPeque').addClass("active");
+	}
+	else {
+		$('#menu').removeClass("menuVisible");
+		$('#nav').removeClass("navegadorDivScroll");
+		$('#nav').removeClass("fixed-top");
+		$('#fondo').removeClass("marginScroll");
+		$('#activeBusquedaScroll, .fa-globe-europe, .idiomaDiv, .fa-caret-down, .facebookLink, .logoPeque').removeClass("active");
+	}
+});
+
+$('#ponerMenu').click(function () {
+	if($('#sidebar').hasClass('active')){
+		$('#sidebar, .menuPalo1, .menuPalo2, .menuPalo3').removeClass('active');
+		$('html').css('overflow-y', 'scroll');
+	}
+	else{
+		$('#sidebar, .menuPalo1, .menuPalo2, .menuPalo3').addClass('active');
+		$('html').css('overflow-y', 'hidden');
+	}
+});
+
+$(document).ready(function () {
+	$('#activarBuscadorMovil, #activarBuscador').on('click', function () {
+			$('#buscadorMovil, .overlay').addClass('active');
+			$('html').css('overflow-y', 'hidden');
+			$('#inputBuscador').focus();
+	});
+	$('#cerrarBuscadorMovil, .overlay').on('click', function () {
+			$('#buscadorMovil, .overlay').removeClass('active');
+			$('html').css('overflow-y', 'scroll');
+	});
+
+	$('.agregar-favoritos').on('click', function () {
+		// leemos los datos clave del producto y los guardamos en un objeto
+		var datos = {
+			id: $(this).attr('id'),
+			url: document.getElementById($(this).attr('id')).value
+		};
+		// guardamos los favoritos en un JSON
+		var favoritos = localStorage.getItem("favoritos") || "[]";
+		favoritos = JSON.parse(favoritos);
+		// buscamos el producto en la lista de favoritos
+		var posLista = favoritos.findIndex(function(e) { return e.id == datos.id; });
+		// eliminamos o guardamos
+		if (posLista > -1) {
+			favoritos.splice(posLista, 1);
+		} else {
+			favoritos.push(datos);
+		}
+		// guardamos la lista de favoritos
+		localStorage.setItem("favoritos", JSON.stringify(favoritos));
+	});
+
+	colorearActivos()
+	function colorearActivos(){
+		// guardamos los favoritos en un JSON
+		var favoritos = localStorage.getItem("favoritos") || "[]";
+		favoritos = JSON.parse(favoritos);
+		for (var x = 0; x < favoritos.length; x++) {
+			$('#' + favoritos[x].id).addClass('activeFav');
+			$('#' + favoritos[x].id).removeClass('desactiveFav');
+		}
+	}
+});
+
+
+$('.favoritosBotonAnadir').click(function () {
+	if($(this).hasClass('desactiveFav')){
+		$(this).addClass('activeFav');
+		$(this).removeClass('desactiveFav');
+	}
+	else{
+		$(this).removeClass('activeFav');
+		$(this).addClass('desactiveFav');
+	}
+});
+
+
+$('#idiomaSelector').change(function(){
+	document.forms["submitIdioma"].submit();
+});
+
+
+$('#m2, #m3, #m4').mouseover(function(){
+		$('.overlay2').addClass('active');
+});
+$('#m2, #m3, #m4').mouseout(function(){
+		$('.overlay2').removeClass('active');
+});
+
+
+if (localStorage.controlcookie > 0){
+	document.getElementById('cookie1').style.display = 'none';
+}
+function controlcookies() {
+	localStorage.controlcookie = (localStorage.controlcookie || 0);
+	localStorage.controlcookie++;
+	cookie1.style.display='none';
+}
