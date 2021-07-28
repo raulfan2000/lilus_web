f09538e8c714457433e4a300e22d06b8e6c31668
Raul Siguenza <rss75@alu.ua.es>
2021-07-28 13:07:05 +0200

    d

--- lilus/inicio/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.db import models
-from django.contrib.auth.models import User
-import inicio.models
-import inspect
-
-for model in inspect.getmembers(inicio.models, inspect.isclass):
-    if models.Model in inspect.getmro(model[1]) and not model[1] is User:
-        admin.site.register(model[1])
--- lilus/inicio/mail.py
+++
@@ -1,7 +0,0 @@
-from lilus.mail import Email
-
-class contactoMail(Email):
-    # En el context se le tiene que pasar plan y every
-    template = 'inicio/plantillasMail/contacto.html'
-    template_text = 'inicio/plantillasMail/contacto.ttx'
-    title = 'Formulario de contacto enviado'
